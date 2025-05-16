c4aebf7a8f2a908a907ddbdea63fdff8c34a359f
Fabian Bouché <fabian.bouche@liferay.com>
2025-05-16 10:15:55 +0200

    I made a mistake

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ const local = process.env.LOCAL_DEV === 'true';
 
 export default defineConfig({
     plugins: [],
-    root: path.resolve('frontend'),
+    root: path.resolve('__dirname'),
     server: {
       open: '/index.html', // opens localhost:5173/index.html
     },
